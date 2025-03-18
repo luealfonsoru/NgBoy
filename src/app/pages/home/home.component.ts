265ee24c0f6512bfcad445c3ccc646124ae9892c
Luis Alfonso <luealfonsoru@gmail.com>
2025-03-17 19:39:30 -0500

    firts operands and improvements over the instruccions object

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ScreenComponent } from '../../components/screen/screen.component';
+import { CPUService } from '../../services/cpu.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,14 @@ import { ScreenComponent } from '../../components/screen/screen.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private cpuService: CPUService) {}
+  ngOnInit(): void {
+    this.cpuService.performInstruction(0x00, []).then(() => {
+      this.cpuService.performInstruction(0x01, [0x00, 0xFF]).then(() => {
+        this.cpuService.performInstruction(0x03, []).then(() => {});
+      })
+    });
+  }
 }
